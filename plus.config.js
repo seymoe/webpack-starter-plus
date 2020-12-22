719b2919b9b847719aa354e54d80f159b71ff727
seymoe <seymoe@yeah.net>
2020-12-22 21:38:14 +0800

    add library target

--- plus.config.js
+++ plus.config.js
@@ -1,4 +1,8 @@
 module.exports = {
+  // 应用类型: app (应用) | library (库)
+  appType: 'library',
+  // 应用名称
+  appName: 'app',
   // 开发语言：js | ts
   languageType: 'ts',
   // 环境
--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,17 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const plusConfig = require('./plus.config')
 
+// 构建库相关的配置
+let libraryConf = {}
+if (plusConfig.appType === 'library') {
+  libraryConf = {
+    filename: `${ plusConfig.appName }.js`,
+    libraryTarget: 'umd',
+    globalObject: 'this',
+    library: plusConfig.appName
+  }
+}
+
 module.exports = {
   mode: plusConfig.NODE_ENV,
   entry: {
@@ -12,14 +23,16 @@ module.exports = {
       : './src/index.js'
   },
   output: {
+    path: path.resolve(__dirname, plusConfig.outDir),
     filename: '[name].bundle.[contenthash].js',
-    path: path.resolve(__dirname, plusConfig.outDir)
+    ...libraryConf
   },
   devtool: plusConfig.NODE_ENV === 'development' ? 'eval-source-map' : 'hidden-source-map',
   devServer: {
     contentBase: plusConfig.outDir,
     hot: true
   },
+  externals: {},
   module: {
     rules: [
       {
@@ -35,7 +48,9 @@ module.exports = {
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: 'Webpack Starter Plus'
+      title: 'Webpack Starter Plus',
+      filename: 'index.html',
+      template: path.resolve(__dirname, './public/index.html')
     }),
     new webpack.HotModuleReplacementPlugin()
   ]
